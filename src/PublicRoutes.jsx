8097f2b375f240ddfe841ae0e7d8fd6b5bd68ca1
Codeexpertzaryab <you@example.com>
2024-09-04 00:28:35 +0500

    chnages

--- src/PublicRoutes.jsx
+++ src/PublicRoutes.jsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import { Navigate, Outlet, useNavigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode'; // Assuming you're using a library like `jwt-decode`
-import Navbar from './components/Navbar';
+import React, { useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import axios from 'axios';
+import Loading from './components/Loading';
 import { useCookies } from 'react-cookie';
 
 const PublicRoutes = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+    const [tokenData, setTokenData] = useState(null);
+    const [cookies, , removeCookie] = useCookies(['user']);
     const token = localStorage.getItem('token');
-    let decodedToken = null;
-    const [cookies] = useCookies(['user']);
     const userData = cookies.user;
     const fieldsCompeleteStatus = userData ? userData?.fieldsCompeleteStatus : null;
-    try {
-        decodedToken = token ? jwtDecode(token) : null;
-    } catch (e) {
-        decodedToken = null;
+
+    const verifyToken = async () => {
+        if (token) {
+            try {
+                const response = await axios.get('https://mmi-mymusicindustry-f5f4aaf34e0e.herokuapp.com/token/verify', {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+
+                if (response.status === 200) {
+                    setIsAuthenticated(true);
+                } else {
+                    setIsAuthenticated(false);
+                    localStorage.removeItem('token');
+                    removeCookie('user');
+
+                }
+            } catch (error) {
+                setIsAuthenticated(false);
+                localStorage.removeItem('token');
+                removeCookie('user');
+
+            }
+        } else {
+            setIsAuthenticated(false);
+            localStorage.removeItem('token');
+            removeCookie('user');
+
+        }
+    };
+
+    useEffect(() => {
+        verifyToken();
+    }, [token]);
+
+    if (isAuthenticated === null) {
+        return <Loading />;
     }
 
-    const auth = { token: token ? true : false };
-
-    if (auth.token) {
+    if (isAuthenticated) {
         if (fieldsCompeleteStatus === false) {
-            return <Navigate to={`/profile/${decodedToken.id}`} />;
+            return <Navigate to={`/profile/${tokenData.id}`} />;
         } else {
             return <Navigate to="/expert" />;
         }
@@ -28,7 +59,6 @@ const PublicRoutes = () => {
 
     return (
         <>
-
             <Outlet />
         </>
     );
