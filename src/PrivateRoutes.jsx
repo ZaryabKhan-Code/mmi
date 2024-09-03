b2fd54d05e6205a0f498ddabec87a4c0e6002c56
Codeexpertzaryab <you@example.com>
2024-09-04 00:22:11 +0500

    changes

--- src/PrivateRoutes.jsx
+++ src/PrivateRoutes.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Loading from './components/Loading';
 
 const PrivateRoutes = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(null);
     const token = localStorage.getItem('token');
-
+    const navigate = useNavigate();
     const verifyToken = async () => {
 
         if (token) {
@@ -15,23 +15,25 @@ const PrivateRoutes = () => {
                 const response = await axios.get('https://mmi-mymusicindustry-f5f4aaf34e0e.herokuapp.com/token/verify', {
                     headers: { Authorization: `Bearer ${token}` },
                 });
-
                 if (response.status === 200) {
                     setIsAuthenticated(true);
                 } else {
                     setIsAuthenticated(false);
+                    navigate('/logout')
                 }
             } catch (error) {
                 setIsAuthenticated(false);
+                navigate('/logout')
             }
         } else {
             setIsAuthenticated(false);
+            navigate('/logout')
+
         }
     };
 
     useEffect(() => {
         verifyToken();
-
     }, [token]);
 
     if (isAuthenticated === null) {
