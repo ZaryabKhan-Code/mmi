9bf33baa6cbc208e572db757b6efc7f5bdb22469
Codeexpertzaryab <you@example.com>
2024-09-10 22:22:12 +0500

    changes

--- src/components/profile/Sessions/ProgressSession.jsx
+++ src/components/profile/Sessions/ProgressSession.jsx
@@ -32,7 +32,7 @@ const ProgressSession = ({ id, type }) => {
                     setData(response.data.session[0]);
                     setAudio(response.data.session[0]?.chatMediaMessage);
                 }
-                console.log(response.data)
+
             } catch (error) {
             }
         }
@@ -72,7 +72,7 @@ const ProgressSession = ({ id, type }) => {
                     <br />with {data.expertUserName}
                 </Typography>
                 {type === 'completed' && (
-                    <Link to={`/expert/${data.expertUserid}`}>
+                    <Link to={`/expert/${data.expertUserId}`}>
                         <Button
                             startIcon={<AddIcon />}
                             className='mt-3'
